f198e62b6a1f19f5ea7856120fef8adbcb36d362
Amin Hasanov <44285676+aminhesenov@users.noreply.github.com>
2024-11-18 11:43:20 +0400

    Paperman

---
+++ Paperman/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Paperman
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            richTextBox1 = new RichTextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(192, 64, 0);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(675, 63);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(257, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(175, 30);
+            label1.TabIndex = 0;
+            label1.Text = "P A P E R M A N";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Silver;
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button1.ImageAlign = ContentAlignment.MiddleRight;
+            button1.Location = new Point(0, 69);
+            button1.Name = "button1";
+            button1.Size = new Size(360, 74);
+            button1.TabIndex = 1;
+            button1.Text = "Read File";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Silver;
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button2.Location = new Point(0, 149);
+            button2.Name = "button2";
+            button2.Size = new Size(360, 67);
+            button2.TabIndex = 2;
+            button2.Text = "Add File";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Silver;
+            button3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button3.Location = new Point(0, 222);
+            button3.Name = "button3";
+            button3.Size = new Size(360, 65);
+            button3.TabIndex = 3;
+            button3.Text = "Remove File";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Silver;
+            button4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            button4.Location = new Point(0, 293);
+            button4.Name = "button4";
+            button4.Size = new Size(360, 65);
+            button4.TabIndex = 4;
+            button4.Text = "DOC File";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Silver;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(283, 83);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(51, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Silver;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(272, 161);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(62, 44);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Silver;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(272, 233);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(62, 44);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Silver;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(272, 302);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(62, 44);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 7;
+            pictureBox4.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(366, 69);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(297, 288);
+            richTextBox1.TabIndex = 8;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 128, 0);
+            ClientSize = new Size(675, 369);
+            Controls.Add(richTextBox1);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Paperman";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ Paperman/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+
+namespace Paperman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Save dialog
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "All Files (*.*)|*.*";
+            saveFile.ShowDialog();
+
+            //Create file
+            File.Create(saveFile.FileName).Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Open dialog           
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "All Files (*.*)|*.*";
+            openFile.ShowDialog();
+
+            //Remove file
+            File.Delete(openFile.FileName);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Save dialog
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "All Files (*.*)|*.*";
+            saveFile.FileName = ".docx";
+            saveFile.ShowDialog();
+
+            //Create file
+            File.Create(saveFile.FileName).Close();
+        }
+        string path = string.Empty;
+        string content = string.Empty;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text Files (*.txt)|*.txt";
+            openFile.ShowDialog();
+
+            //Path read
+            path = openFile.FileName;
+
+            //Content read
+            content = File.ReadAllText(path);
+
+
+            //Write richtext
+            richTextBox1.Text = content;
+        }
+    }
+}
